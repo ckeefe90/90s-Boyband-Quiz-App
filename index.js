8cb581e48a7b35dde91ca932c1865e7551c5ce57
Caitlin Keefe <caitlinkeefe@thinkful.com>
2020-07-23 20:54:08 -0400

    pictures

---
+++ index.js
@@ -0,0 +1,114 @@
+const STORE = {
+    questions: [{ //1
+            question: "Which boy band was one of the first & most popular?",
+            options: [
+                "NSYNC",
+                "98 DEGREES",
+                "BACKSTREET BOYS",
+                "NEW KIDS ON THE BLOCK"
+            ],
+            answer: "NEW KIDS ON THE BLOCK "
+        },
+        { //2 
+            question: "Who were the members of BACKSTREET BOYS?",
+            options: [
+                "Kevin Richardson, Jordan Knight, Joey McIntyre, Nick Carter, Jesse McCartney",
+                "AJ McLean, Joey McIntyre, Brian Litrell, Lance Bass, Justin Jeffre",
+                "Howie Dorough, Nick Carter, AJ McLean, Kevin Richardson, Brian Litrell",
+                "Jordan Knight, Jeff Timons, Kevin Richardson, Justin Jeffre, Drew Lachey"
+            ],
+            answer: "Howie Dorough, Nick Carter, AJ McLean, Kevin Richardson, Brian Litrell",
+            img: "backstreetboys.jpg"
+        },
+        { //3
+            question: "Who sang “New Kids on the Block had a bunch of hits, Chinese food makes me sick, And I think it’s fly when girls stop by, For the summer, for the summer”?",
+            options: [
+                "98 DEGREES",
+                "BACKSTREET BOYS",
+                "LFO",
+                "O-TOWN"
+            ],
+            answer: "LFO",
+            img: "SummerGirlsLFO.jpg"
+        },
+        { //4
+            question: "Who were the members of NSYNC?",
+            options: [
+                "Chris Kirkpatrick, Jesse McCartney, Joey Fatone, Lance Bass, AJ McLean",
+                "Chris Kirkpatrick, JC Chasez, Joey Fatone, Lance Bass, Justin Timberlake",
+                "Brian Litrell, Justin Timberlake, Kevin Richardson, Joey Fatone, JC Chasez",
+                "Jordan Knight, JC Chasez, Howie Dorough, Chris Kirkpatrick,Justin Timberlake"
+            ],
+            answer: "Chris Kirkpatrick, JC Chasez, Joey Fatone, Lance Bass, Justin Timberlake",
+            img: "nsync2.jpg"
+        },
+        { //5
+            question: 'Who sang “Tell me why, Ain’t nothin’ but a heartache, Tell me why, Ain’t nothin’ but a mistake, Tell me why, I never want to hear you say, I want it that way”?',
+            options: [
+                "O-TOWN",
+                "NEW KIDS ON THE BLOCK",
+                "BACKSTREET BOYS",
+                "DREAM STREET"
+            ],
+            answer: "BACKSTREET BOYS",
+            img: "bsb.jpg"
+        },
+        { //6
+            question: "Who were the members of 98 Degrees?",
+            options: [
+                "Jason Orange, Howie Dorough,Jeff Timmons, Brian Litrell",
+                "Drew Lachey, Nick Lachey, Justin Jeffre, Jeff Timmons",
+                "Kevin Richardson, Jeff Timmons, Chris Kirkpatrick, Justin Jeffre",
+                "Nick Lachey, Drew Lachey, Joey McIntyre, Jordan Knight"
+            ],
+            answer: "Drew Lachey, Nick Lachey, Justin Jeffre, Jeff Timmons",
+            img: "90degrees2.jpg"
+        },
+        { //7
+            question: "Who sang the hit song “Bye Bye Bye”?",
+            options: [
+                "BACKSTREET BOYS",
+                "NSYNC",
+                "DREAM STREET",
+                "NEW KIDS ON THE BLOCK"
+            ],
+            answer: "NSYNC",
+            img: "nsync.jpg"
+        },
+        { //8
+            question: "Who were the members of New Kids on The Block?",
+            options: [
+                "Mark Wahlberg, Jordan Knight, Jonathan Knight, Howie Dorough, Justin Jeffre",
+                "Donnie Wahlberg, Justin Jeffre, Joey McIntyre, Lance Bass, Ralph Tresvant",
+                "Jordan Knight, Danny Wood, Joey McIntyre, Jonathan Knight, Donnie Wahlberg",
+                "Danny Wood, Lance Bass, Kevin Richardson, Jesse McCartney, Matt Ballinger"
+            ],
+            answer: "Jordan Knight, Danny Wood, Joey McIntyre, Jonathan Knight, Donnie Wahlberg",
+            img: "newkids.jpg"
+        },
+        { //9
+            question: "Who sang “It’s the hardest thing I’ll ever have to do, To look you in the eye and tell you I don’t love you”?",
+            options: [
+                "DREAM STREET",
+                "LFO",
+                "98 DEGREES",
+                "BACKSTREET BOYS"
+            ],
+            answer: "98 DEGREES",
+            img: "98Degrees.jpg"
+        },
+        { //10
+            question: "Which boy band member famously dated Britney Spears then later wrote a hit song about their breakup for his debut solo album?",
+            options: [
+                "Kevin Richardson, BACKSTREET BOYS",
+                "JC Chasez, NSYNC",
+                "Ashley Parker Angel, O-TOWN",
+                "Justin Timberlake, NSYNC"
+            ],
+            answer: "Justin Timberlake, NSYNC",
+            img: "jtbritney.jpg"
+        },
+    ],
+};
+
+//
